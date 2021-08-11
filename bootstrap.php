63f6c4b75edbf546041b456f5faa3f5346391f1b
Mohammad Shahbaz Alam <shahbaz@magic.link>
2021-08-11 20:17:07 +0530

    Remove phpunit files and entry

--- bootstrap.php
+++ bootstrap.php
@@ -6,5 +6,3 @@ WP_Mock::bootstrap();
 
 require_once __DIR__ . '/includes/class-admin.php';
 require_once __DIR__ . '/includes/class-login.php';
-require_once __DIR__ . '/tests/WP_User.php';
-require_once __DIR__ . '/tests/WP_Error.php';
